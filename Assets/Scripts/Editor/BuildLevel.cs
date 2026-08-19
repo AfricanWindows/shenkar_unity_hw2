99586728838f33321116ddde7933e0ca2151aafe
Alan Zirkiev <zirkiev555@gmail.com>
2026-08-19 13:59:38 +0300

    update and install editor and scripting, starting to make map with tiles

---
+++ Assets/Scripts/Editor/BuildLevel.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEditor;
+
+public class BuildLevel : EditorWindow
+{
+    [MenuItem("Tools/Level Creator")]
+    public static void ShowWindow()
+    {
+        
+    }
+}
